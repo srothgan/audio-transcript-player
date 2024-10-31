03ec3daef2596154bb84ec3d944195ee28c98c74
Simon Rothgang <srothgan@uni-muenster.de>
2024-10-31 19:27:27 +0100

    navbar redesign

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,16 +1,20 @@
-import GitHubLink from "./GitHubLink"
-import Link from "next/link"
-export default function Navbar(){
+import GitHubLink from "./GitHubLink";
+import Link from "next/link";
 
+export default function Navbar() {
+  return (
+    <navbar className='w-full p-1 md:p-2 xl:p-2'>
+    <div className="w-full flex items-center justify-between p-6 bg-gradient-to-r from-slate-600 to-slate-800 text-white shadow-md rounded-lg">
+      
+      {/* Logo and Title */}
+      <Link href="/" className="flex flex-col gap-1 italic">
+        <h4 className="text-xl font-semibold tracking-wide">Audio Player</h4>
+        <p className="text-sm text-slate-300">For Transcription</p>
+      </Link>
 
-    return(
-        <div className='w-full flex items-center justify-between p-4 bg-white filter drop-shadow-xl '>
-            <Link href="/" className='flex flex-col gap-2 italic'>
-                <h4>Audio Player</h4>
-                <p>For Transcription</p>
-            </Link>
-            
-            <GitHubLink/>
-        </div>
-    )
+      {/* GitHub Icon Link */}
+      <GitHubLink />
+    </div>
+    </navbar>
+  );
 }
