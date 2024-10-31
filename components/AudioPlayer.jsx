32af3ae8f7b4c684a4e92e6602b3bfa35b4cc7ef
Simon Rothgang <srothgan@uni-muenster.de>
2024-10-31 22:23:33 +0100

    moved play, pause, back and skip buttons into single component

--- components/AudioPlayer.jsx
+++ components/AudioPlayer.jsx
@@ -1,13 +1,13 @@
 // components/AudioPlayer.js
 "use client";
 import { useRef, useState, useEffect } from "react";
-import { FaPlay, FaPause, FaCopy} from "react-icons/fa";
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import DeleteButton from "./AudioPlayer/DeleteButton";
 import PlaybackSpeed from "./AudioPlayer/PlaybackSpeed";
 import VolumeBar from "./AudioPlayer/VolumeBar";
 import CopyClipboardButton from "./AudioPlayer/CopyClipboardButton";
+import ActionBar from "./AudioPlayer/ActionBar";
 
 const AudioPlayer = () => {
   const audioRef = useRef(null);
@@ -275,57 +275,7 @@ const AudioPlayer = () => {
             </div>
 
             {/* Play, stop, skip buttons */}
-            <div className="flex w-full md:w-1/2 justify-between">
-              <button
-                type="button"
-                onClick={() => skipTime(-10)}
-                className="hidden md:flex px-2 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 transition"
-              >
-                -10s
-              </button>
-              <button
-                type="button"
-                onClick={() => skipTime(-5)}
-                className="px-2 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 transition"
-              >
-                -5s
-              </button>
-              <button
-                type="button"
-                onClick={() => skipTime(-1)}
-                className="px-2 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 transition"
-              >
-                -1s
-              </button>
-              <button
-                type="button"
-                onClick={togglePlay}
-                className="px-4 py-2 bg-blue-500 text-white rounded-lg font-medium hover:bg-blue-600 transition"
-              >
-                {isPlaying ? <FaPause /> : <FaPlay />}
-              </button>
-              <button
-                type="button"
-                onClick={() => skipTime(1)}
-                className="px-2 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 transition"
-              >
-                +1s
-              </button>
-              <button
-                type="button"
-                onClick={() => skipTime(5)}
-                className="px-2 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 transition"
-              >
-                +5s
-              </button>
-              <button
-                type="button"
-                onClick={() => skipTime(10)}
-                className="hidden md:flex px-2 py-2 bg-gray-200 rounded-lg hover:bg-gray-300 transition"
-              >
-                +10s
-              </button>
-            </div>
+            <ActionBar isPlaying={isPlaying} togglePlay={togglePlay} skipTime={skipTime}/>
 
             {/* Volume Control */}
             <VolumeBar volume={volume} handleVolumeChange={handleVolumeChange}/>
